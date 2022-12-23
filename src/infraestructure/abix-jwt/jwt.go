5c54842cf52ee76e6537bb5e4de1ab901c45edbd
Enoc Martinez <enoc.martinez@pulzo.com>
2022-12-23 15:29:22 -0500

    JWT

--- src/infraestructure/abix-jwt/jwt.go
+++ src/infraestructure/abix-jwt/jwt.go
@@ -57,7 +57,7 @@ func GenerateJWT(email, role string) (string, error) {
 	claims["authorized"] = true
 	claims["email"] = email
 	claims["role"] = role
-	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+	claims["exp"] = time.Now().Add(time.Minute * 120).Unix()
 
 	tokenString, err := token.SignedString(mySigningKey)
 
